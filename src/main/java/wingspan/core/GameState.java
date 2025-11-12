80461afb4a7860a213e9be53bb267bdce0ca9a46
ZhuAndrewK2209615 <K2209615@students.katyisd.org>
2025-11-11 20:37:32 -0600

    Update GameState.java

--- src/main/java/wingspan/core/GameState.java
+++ src/main/java/wingspan/core/GameState.java
@@ -3,6 +3,7 @@ package wingspan.core;
 import java.util.*;
 import wingspan.food.*;
 import wingspan.cards.*;
+import wingspan.enums.Food;
 
 public class GameState {
 	//these are initialized in WingspanRunner's main method
@@ -10,12 +11,19 @@ public class GameState {
 	public static CardManager cardManager;
 	public static ArrayList<Player> players;
 	public static GoalBoard goalBoard;
-	public static Player activePlayer;
+	public static Player activePlayer; //current turn's player
 
-	//variables containing players with the least cards in the respective habitats - used for PowerBehavior logic
-	public static ArrayList<Player> leastGrasslands;
-	public static ArrayList<Player> leastWetlands;
-	public static ArrayList<Player> leastForest;
+	//variables used for PowerBehavior logic
+	public static ArrayList<Player> abilityPlayers = new ArrayList<Player>(); /* 
+		when an ability is activated, this ArrayList will contain all players that are involved in that ability.
+		e.g., if the ability is "player with least wetland cards draw a card", this ArrayList would contain all players that have the least amount of wetland cards
+		put players in this list based on player number order
+	*/
+	public static ArrayList<Card> cardsToDraw = new ArrayList<Card>(); //contains the cards a player chose to draw for an ability
+	public static ArrayList<Card> cardsToTuck = new ArrayList<Card>(); //contains the cards a player chose to tuck for an ability
+	//if multiple players are involved in one of these abilities, put cards in these lists in player number order (eg. player 1's cards in the list come before player 3's)
+
+	public static Food foodChoice; //if an ability allows the player to choose any food, it will be stored here.
 
 	public static boolean activePlayerTuckedCard;
 	public static Card activeCard; // contains the card whose power is currently being executed
