6b067e5245ad168df222727c988b383fdcf050d1
PeluffoMarceloK1714016 <K1714016@students.katyisd.org>
2025-11-13 08:23:12 -0600

    method: execute previous brown power

--- src/main/java/wingspan/cards/behavior/PlayPreviousBrownBehavior.java
+++ src/main/java/wingspan/cards/behavior/PlayPreviousBrownBehavior.java
@@ -5,6 +5,7 @@ import java.util.List;
 import wingspan.cards.Card;
 import wingspan.core.GameState;
 import wingspan.core.Player;
+import wingspan.enums.Color;
 
 public class PlayPreviousBrownBehavior implements PowerBehavior {
     private int numPowers;
@@ -17,11 +18,22 @@ public class PlayPreviousBrownBehavior implements PowerBehavior {
 
 
     @Override
-    public boolean executePower() {
+    public boolean executePower() 
+    {
         // play previous brown power behavior
         Player activePlayer = GameState.activePlayer;
         List<Card> activeHabitatCards = activePlayer.getGameBoard().getActiveHabitat();
-        
+        Card activeCard = GameState.activeCard;
+
+        for (int i = activeHabitatCards.indexOf(activeCard) + 1; i < activeHabitatCards.size(); i++)
+        {
+            Card currentCard = activeHabitatCards.get(i);
+            if (currentCard.getBirdInfo().getPowerColor() == Color.BROWN)
+            {
+                currentCard.getBirdInfo().getBehavior().executePower();
+            }
+            
+        }
 
         return true;
     }
