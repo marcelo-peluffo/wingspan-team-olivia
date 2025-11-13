268047f310abcb73e593a7b8115331950a58659e
PeluffoMarceloK1714016 <K1714016@students.katyisd.org>
2025-11-13 15:46:48 -0600

    streamlined test file

--- src/main/java/wingspan/BehaviorFactoryTest.java
+++ src/main/java/wingspan/BehaviorFactoryTest.java
@@ -5,9 +5,6 @@ import wingspan.cards.*;
 public class BehaviorFactoryTest {
     public static void main(String[] args) {
         DataTable.initialize();
-        for (BirdInfo birdInfo : CardManager.birdInfos)
-        {
-            System.out.println(birdInfo);
-        }
+        System.out.println(CardManager.birdInfos);
     }
 }
