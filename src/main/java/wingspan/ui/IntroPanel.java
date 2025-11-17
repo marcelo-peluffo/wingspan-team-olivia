bbb8c12cc388022c3f4efd697ed0891c53b9a4d8
Khare, Rishi- period 5 <K2307266@students.katyisd.org>
2025-11-17 08:25:07 -0600

    Update IntroPanel.java

--- src/main/java/wingspan/ui/IntroPanel.java
+++ src/main/java/wingspan/ui/IntroPanel.java
@@ -1,22 +1,48 @@
-package wingspan.ui;
-import java.awt.*;
-import javax.swing.*;
-import wingspan.core.GameState;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+//import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
 public class IntroPanel extends JPanel{
     
+	private BufferedImage intro, playButton;
 
-    public IntroPanel()
-    {
-
+    public IntroPanel(){
+    	try {
+    		
+    		intro = ImageIO.read(IntroPanel.class.getResource("/Image/IntroImage.jpg"));
+    		playButton = ImageIO.read(IntroPanel.class.getResource("/Image/PlayButton.png"));
+    		
+    		
+    	}
+    	catch(Exception e){
+    		System.out.println("Exception error");
+    		return;
+    	}
     }
 
     public void paint(Graphics g)
     {
         super.paint(g);
-        if (!GameState.gameStarted)
+        
+        Graphics2D g2d = (Graphics2D) g;
+        
+        g2d.setColor(Color.green);
+        BasicStroke stroke = new BasicStroke(10);
+        g2d.setStroke(stroke);
+        g2d.drawRect(0, 0, 1920, 1080);
+        g2d.drawImage(intro, 5, 5, 1910, 1070,null);
+        g2d.drawImage(playButton, getWidth()/2-200, getHeight()/2, 300,100, null);
+        g2d.drawString("PLAY!", getWidth()/2-100, getHeight()/2+50);
+        /*if (!GameState.gameStarted)
         {
             
-        }
+        }*/
     }
 }
