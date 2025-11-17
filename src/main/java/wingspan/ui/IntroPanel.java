0fe56ae214cc04e8495f3748c96a6ace9cf1651d
Khare, Rishi- period 5 <K2307266@students.katyisd.org>
2025-11-17 08:34:15 -0600

    Add font styling to 'PLAY!' text in IntroPanel

--- src/main/java/wingspan/ui/IntroPanel.java
+++ src/main/java/wingspan/ui/IntroPanel.java
@@ -1,5 +1,6 @@
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
@@ -31,15 +32,19 @@ public class IntroPanel extends JPanel{
     {
         super.paint(g);
         
+        Font font = new Font("Serif", Font.PLAIN, 50);
+        
         Graphics2D g2d = (Graphics2D) g;
         
         g2d.setColor(Color.green);
+        g2d.setFont(font);
         BasicStroke stroke = new BasicStroke(10);
         g2d.setStroke(stroke);
         g2d.drawRect(0, 0, 1920, 1080);
         g2d.drawImage(intro, 5, 5, 1910, 1070,null);
         g2d.drawImage(playButton, getWidth()/2-200, getHeight()/2, 300,100, null);
-        g2d.drawString("PLAY!", getWidth()/2-100, getHeight()/2+50);
+        g2d.setColor(Color.BLACK);	
+        g2d.drawString("PLAY!", getWidth()/2-110, getHeight()/2+60);
         /*if (!GameState.gameStarted)
         {
             
