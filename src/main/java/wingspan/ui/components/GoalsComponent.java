bc6a4576c47c2f202d73bf161275022124269937
Glitched Forever <K2209615@students.katyisd.org>
2025-11-17 20:35:25 -0600

    finished GoalsComponent UI

---
+++ src/main/java/wingspan/ui/components/GoalsComponent.java
@@ -0,0 +1,32 @@
+package wingspan.ui.components;
+
+import javax.swing.JPanel;
+import java.awt.*;
+import wingspan.cards.goals.*;
+import wingspan.core.*;
+
+public class GoalsComponent extends JPanel
+{
+    private Goal[] goals;
+    public GoalsComponent()
+    {
+        goals = GameState.goalBoard.getGoals();
+    }
+
+    public void paint(Graphics g)
+    {
+        super.paint(g);
+        int goalXPos = getWidth() - 400;
+        for(Goal goal: goals)
+        {
+            g.drawImage(goal.getImage(), goalXPos, getHeight() - 100, 100, 100, null);
+            goalXPos += 100;
+        }
+        int arrowXPos = getWidth() - 450 + GameState.roundNum * 100;
+        int[] xPoints = {arrowXPos - 50, arrowXPos, arrowXPos + 50};
+        int[] yPoints = {getHeight() - 155, getHeight() - 110, getHeight() - 155};
+        g.fillPolygon(xPoints, yPoints, 3);
+        g.setFont(new Font("Arial", Font.BOLD, 30));
+        g.drawString("Goals", getWidth() - 250, getHeight() - 160);
+    }
+}
