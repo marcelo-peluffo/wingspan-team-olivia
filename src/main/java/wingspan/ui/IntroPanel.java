3114abafab3f0f7b5747c86ff0eb2997587c5766
Khare, Rishi- period 5 <K2307266@students.katyisd.org>
2025-11-17 19:00:28 -0600

    Add Wingspan logo and adjust layout in IntroPanel

--- src/main/java/wingspan/ui/IntroPanel.java
+++ src/main/java/wingspan/ui/IntroPanel.java
@@ -12,13 +12,14 @@ import javax.swing.JPanel;
 
 public class IntroPanel extends JPanel{
     
-	private BufferedImage intro, playButton;
+	private BufferedImage intro, playButton, WingspanImage;
 
     public IntroPanel(){
     	try {
     		
     		intro = ImageIO.read(IntroPanel.class.getResource("/Image/IntroImage.jpg"));
     		playButton = ImageIO.read(IntroPanel.class.getResource("/Image/PlayButton.png"));
+    		WingspanImage = ImageIO.read(IntroPanel.class.getResource("/Image/LogoImage.png"));
     		
     		
     	}
@@ -32,7 +33,7 @@ public class IntroPanel extends JPanel{
     {
         super.paint(g);
         
-        Font font = new Font("Serif", Font.PLAIN, 50);
+        Font font = new Font("Serif", Font.BOLD, 50);
         
         Graphics2D g2d = (Graphics2D) g;
         
@@ -40,11 +41,13 @@ public class IntroPanel extends JPanel{
         g2d.setFont(font);
         BasicStroke stroke = new BasicStroke(10);
         g2d.setStroke(stroke);
-        g2d.drawRect(0, 0, 1920, 1080);
-        g2d.drawImage(intro, 5, 5, 1910, 1070,null);
-        g2d.drawImage(playButton, getWidth()/2-200, getHeight()/2, 300,100, null);
+        //dg2d.drawRect(0, 0, 1920, 1080);
+        g2d.drawImage(intro, 0, 0, 1920, 1080,null);
+        g2d.drawImage(playButton, getWidth()/2-200, getHeight()/2+300, 300,100, null);
         g2d.setColor(Color.BLACK);	
-        g2d.drawString("PLAY!", getWidth()/2-110, getHeight()/2+60);
+        g2d.drawString("PLAY!", getWidth()/2-110, getHeight()/2+60+300);
+        g2d.drawImage(WingspanImage, getWidth()/2-200, 400, null);
+        //g2d.fillRect(1540, 0, 400, 400);
         /*if (!GameState.gameStarted)
         {
             
