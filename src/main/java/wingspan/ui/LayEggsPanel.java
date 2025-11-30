575993bc0a4b7ad313d34404ec4a29fa4edc3def
PeluffoMarceloK1714016 <K1714016@students.katyisd.org>
2025-11-29 19:59:00 -0600

    possible new skeleton

--- src/main/java/wingspan/ui/LayEggsPanel.java
+++ src/main/java/wingspan/ui/LayEggsPanel.java
@@ -7,82 +7,83 @@ import java.awt.event.*;
 import java.awt.image.*;
 import java.io.*;
 import java.util.*;
-
 import javax.imageio.ImageIO;
-
 import wingspan.core.*;
 
 public class LayEggsPanel implements KeyListener {
     private GameBoard gameBoard;
     private BufferedImage plus;
     private Habitat selectedHabitat;
+    private int selectedCardIndex = 0; // index of card within the habitat
 
     public LayEggsPanel() {
         gameBoard = GameState.activePlayer.getGameBoard();
         selectedHabitat = Habitat.GRASSLANDS;
+
         try {
-            plus = ImageIO.read(getClass().getResource("/Images/Plus.jpg")); // plus image for clicking
+            plus = ImageIO.read(getClass().getResource("/Images/Plus.jpg"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void paint(Graphics g) {
-        int tempX = 0; // we need to figure out coordinates for each bird card
+        int tempX = 0; 
         int tempY = 0;
 
-        for (Card card : gameBoard.returnAllCards()) { // show all options
+        List<Card> cards = gameBoard.getCardsInHabitat(selectedHabitat);
+
+        for (int i = 0; i < cards.size(); i++) {
+            Card card = cards.get(i);
             g.drawImage(plus, tempX, tempY, null);
             g.drawString(String.valueOf(card.getCurrentEggs()), tempX, tempY);
-        }
-    }
 
+            if (i == selectedCardIndex) {
+                // highlight selected card
+                g.setColor(Color.RED);
+                g.drawRect(tempX - 2, tempY - 2, plus.getWidth() + 4, plus.getHeight() + 4);
+                g.setColor(Color.BLACK);
+            }
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_UP:
-                boolean canGoUpOne = (selectedHabitat == Habitat.GRASSLANDS);
-                boolean canGoUpTwo = (selectedHabitat == Habitat.WETLANDS);
-
-                if (!canGoUpTwo || !canGoUpOne) {
-                    break; // can't go up or down
-                }
-
-                if (canGoUpTwo) {
-                    selectedHabitat = Habitat.GRASSLANDS;
-                }
-
-                if (canGoUpOne) {
-                    selectedHabitat = Habitat.FOREST;
-                }
-
-                break;
-
-            case KeyEvent.VK_DOWN:
-                System.out.println("Down arrow");
-                break;
-
-            case KeyEvent.VK_LEFT:
-                System.out.println("Left arrow");
-                break;
-
-            case KeyEvent.VK_RIGHT:
-                System.out.println("Right arrow");
-                break;
+            tempX += plus.getWidth() + 10; // move to next card
         }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                if (selectedHabitat == Habitat.GRASSLANDS) selectedHabitat = Habitat.FOREST;
+                else if (selectedHabitat == Habitat.WETLANDS) selectedHabitat = Habitat.GRASSLANDS;
+                selectedCardIndex = 0;
+                break;
+
+            case KeyEvent.VK_DOWN:
+                if (selectedHabitat == Habitat.FOREST) selectedHabitat = Habitat.GRASSLANDS;
+                else if (selectedHabitat == Habitat.GRASSLANDS) selectedHabitat = Habitat.WETLANDS;
+                selectedCardIndex = 0;
+                break;
+
+            case KeyEvent.VK_LEFT:
+                if (selectedCardIndex > 0) selectedCardIndex--;
+                break;
+
+            case KeyEvent.VK_RIGHT:
+                List<Card> cards = gameBoard.getCardsInHabitat(selectedHabitat);
+                if (selectedCardIndex < cards.size() - 1) selectedCardIndex++;
+                break;
+        }
+        repaintPanel();
+    }
+
+    private void repaintPanel() {
+        // replace with whatever triggers repaint in your UI
+        // e.g., if this is a JPanel: this.repaint();
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
-    }
+    public void keyReleased(KeyEvent e) { }
 
+    @Override
+    public void keyTyped(KeyEvent e) { }
 }
