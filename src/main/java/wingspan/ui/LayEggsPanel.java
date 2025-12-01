77249accccb0f0b3b6e97f3863ee9ae8a84e6e80
PeluffoMarceloK1714016 <K1714016@students.katyisd.org>
2025-12-01 08:13:40 -0600

    add board

--- src/main/java/wingspan/ui/LayEggsPanel.java
+++ src/main/java/wingspan/ui/LayEggsPanel.java
@@ -1,7 +1,10 @@
 package wingspan.ui;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
@@ -24,6 +27,8 @@ public class LayEggsPanel extends JPanel implements KeyListener {
     private Habitat selectedHabitat;
     private int selectedCardIndex = 0; // index of card within the habitat
     private HashMap<Card, Pair> cardPositions;
+    private final int CARD_WIDTH = 125;
+    private final int CARD_HEIGHT = 200;
 
     public LayEggsPanel() {
         gameBoard = GameState.activePlayer.getGameBoard();
@@ -37,33 +42,83 @@ public class LayEggsPanel extends JPanel implements KeyListener {
     }
 
     public void paint(Graphics g) {
-        // int tempX = 0; // fix to actual coordinates later
-        // int tempY = 0;
-        super.paint(g);
+        try {
+            // int tempX = 0; // fix to actual coordinates later
+            // int tempY = 0;
+            super.paint(g);
+            int playerIndex = GameState.players.indexOf(GameState.activePlayer);
+            Graphics2D g2d = (Graphics2D)g;
+            BufferedImage boardImage = ImageIO.read(MainPanel.class.getResource("/Images/GameBoard.jpg"));
+            BufferedImage displayedCard = null;
+            List<Card> forestCards = gameBoard.getForest();
+            List<Card> grasslandsCards = gameBoard.getGrasslands();
+            List<Card> wetlandsCards = gameBoard.getWetlands();
 
 
-
-        // begin drawing the plus
-        List<Card> cards = gameBoard.getCardsInHabitat(selectedHabitat);
-        System.out.println(cardPositions);
-        for (int i = 0; i < cards.size(); i++) {
-
-            Card card = cards.get(i);
-            Pair pair = cardPositions.get(card);
-
-            if (card.getCurrentEggs() < card.getBirdInfo().getMaxEggs()) { // only draw (+) if more eggs can be added to bird
-                g.drawImage(plus, pair.getX(), pair.getY(), null);
+            g.drawImage(boardImage, 350, 175, (int)(getWidth() * 0.6), (int)(getHeight() * 0.65), null);
+            g2d.setColor(Color.BLACK);
+            g2d.setStroke(new BasicStroke(7));
+            g2d.drawRect(getWidth() / 2 - 255, 120, 409, 50);
+            g2d.setColor(Color.LIGHT_GRAY);
+            g2d.fillRect(getWidth() / 2 - 251, 124, 402, 51);
+            g2d.setColor(Color.BLACK);
+            g2d.drawLine(getWidth() / 2 - 200, 127, getWidth() / 2 - 200, 172);
+            g2d.drawLine(getWidth() / 2 + 100, 127, getWidth() / 2 + 100, 172);
+            int[] xPoints2 = {getWidth() / 2 - 240, getWidth() / 2 - 220, getWidth() / 2 - 220};
+            int[] yPoints2 = {150, 135, 165};
+            g2d.fillPolygon(xPoints2, yPoints2, 3);
+            int[] xPoints3 = {getWidth() / 2 + 140, getWidth() / 2 + 120, getWidth() / 2 + 120};
+            int[] yPoints3 = {150, 135, 165};
+            g2d.fillPolygon(xPoints3, yPoints3, 3);
+            g.setFont(new Font("Arial", Font.BOLD, 20));
+            g.drawString("Change board to view", getWidth() / 2 - 150, 155);
+            int xPos = 610;
+            for(Card c: forestCards)
+            {
+                g.drawImage(c.getCardImage(), xPos, 210, CARD_WIDTH, CARD_HEIGHT, null);
+                cardPositions.put(c, new Pair(xPos, 210));
+                xPos += 175;
             }
-            g.drawString(card.getCurrentEggs() + " / " + card.getBirdInfo().getMaxEggs(), pair.getX(), pair.getY());
-
-            if (i == selectedCardIndex) {
-                // highlight selected card
-                g.setColor(Color.RED);
-                g.drawRect(pair.getX() - 2, pair.getY() - 2, plus.getWidth() + 4, plus.getHeight() + 4);
-                g.setColor(Color.BLACK);
+            xPos = 610;
+            for(Card c: grasslandsCards)
+            {
+                g.drawImage(c.getCardImage(), xPos, 425, CARD_WIDTH, CARD_HEIGHT, null);
+                cardPositions.put(c, new Pair(xPos, 425));
+                xPos += 175;
             }
-
-            //tempX += plus.getWidth() + 10; // move to next card
+            xPos = 610;
+            for(Card c: wetlandsCards)
+            {
+                g.drawImage(c.getCardImage(), xPos, 640, CARD_WIDTH, CARD_HEIGHT, null);
+                cardPositions.put(c, new Pair(xPos, 640));
+                xPos += 175;
+            }
+            g.drawImage(displayedCard, 1600, 300, CARD_WIDTH * 2, CARD_HEIGHT * 2, null);
+            
+            
+            // begin drawing the plus
+            List<Card> cards = gameBoard.getCardsInHabitat(selectedHabitat);
+            System.out.println(cardPositions);
+            for (int i = 0; i < cards.size(); i++) {
+                
+                Card card = cards.get(i);
+                Pair pair = cardPositions.get(card);
+                
+                if (card.getCurrentEggs() < card.getBirdInfo().getMaxEggs()) { // only draw (+) if more eggs can be added to bird
+                    g.drawImage(plus, pair.getX(), pair.getY(), null);
+                }
+                g.drawString(card.getCurrentEggs() + " / " + card.getBirdInfo().getMaxEggs(), pair.getX(), pair.getY());
+                
+                if (i == selectedCardIndex) {
+                    // highlight selected card
+                    g.setColor(Color.RED);
+                    g.drawRect(pair.getX() - 2, pair.getY() - 2, plus.getWidth() + 4, plus.getHeight() + 4);
+                    g.setColor(Color.BLACK);
+                }
+                
+                //tempX += plus.getWidth() + 10; // move to next card
+            }
+        } catch (IOException ex) {
         }
     }
 
